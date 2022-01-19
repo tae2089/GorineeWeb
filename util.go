7619085138b9b0b5474b015faf166ef756c09858
tae2089 <tae2089002@gmail.com>
2022-01-19 19:45:41 +0900

    add util function - interface to string

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"unsafe"
 )
 
@@ -10,17 +11,24 @@ func GetString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-func InterfaceConvertIntType(v interface{}) int {
-	return v.(int)
-}
-
-func InterfaceConvertStringType(v interface{}) string {
-	return v.(string)
-}
-
-func InterfaceConvertFloat32Type(v interface{}) float32 {
-	return v.(float32)
-}
-func InterfaceConvertFloat64Type(v interface{}) float64 {
-	return v.(float64)
+func InterfaceConvertString(arg interface{}) string {
+	//       ↓ 인터페이스에 저장된 타입에 따라 case 실행
+	switch arg.(type) {
+	case int: // arg가 int형이라면
+		i := arg.(int)         // int형으로 값을 가져옴
+		return strconv.Itoa(i) // strconv.Itoa 함수를 사용하여 i를 문자열로 변환
+	case float32: // arg가 float32형이라면
+		f := arg.(float32) // float32형으로 값을 가져옴
+		return strconv.FormatFloat(float64(f), 'f', -1, 32)
+		// strconv.FormatFloat 함수를 사용하여 f를 문자열로 변환
+	case float64: // arg가 float64형이라면
+		f := arg.(float64) // float64형으로 값을 가져옴
+		return strconv.FormatFloat(f, 'f', -1, 64)
+		// strconv.FormatFloat 함수를 사용하여 f를 문자열로 변환
+	case string: // arg가 string이라면
+		s := arg.(string) // string으로 값을 가져옴
+		return s          // string이므로 그대로 리턴
+	default:
+		return "Error"
+	}
 }
